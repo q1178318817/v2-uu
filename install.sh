2c1b8ad67a6f1120cf8aa9058fe12dd54468e1f9
q1178318817 <44406327+q1178318817@users.noreply.github.com>
2020-09-05 03:34:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -203,9 +203,9 @@ install_v2-ui() {
             exit 1
         fi
     else
-        last_version=$1
+        last_version=$6
         url="https://github.com/sprov065/v2-ui/releases/download/${last_version}/v2-ui-linux.tar.gz"
-        echo -e "开始安装 v2-ui v$1"
+        echo -e "开始安装 v2-ui v$6"
         wget -N --no-check-certificate -O /usr/local/v2-ui-linux.tar.gz ${url}
         if [[ $? -ne 0 ]]; then
             echo -e "${red}下载 v2-ui v$1 失败，请确保此版本存在${plain}"
